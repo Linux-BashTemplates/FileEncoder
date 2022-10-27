24fb36ba43602817bcb4d09790dc3ee455a6cbec
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2022-10-27 14:21:04 +0300

    Add files via upload

---
+++ encode.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -n "$1" ]
+then
+    FROM_ENCODING="utf-8"
+    TO_ENCODING="UTF-8"
+    OUTPUT="output_"
+    CONVERT=" iconv -f $FROM_ENCODING -t $TO_ENCODING"
+	for file in *.txt; do
+		$CONVERT -f $1 -o $1%.txt.utf8.converted
+	  done
+	exit 0
+else
+    echo "Incorrect agrument. Abort"
+    echo "Usage: ./encode.sh <path/<filename>.txt>"
+fi
