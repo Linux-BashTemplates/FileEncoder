093ff6924c3098a6572e2e7d7ed46e878c808346
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2022-10-27 17:48:00 +0300

    Refact  my old script
    Universal solution for convert your text to unicode

--- encode.sh
+++ encode.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
-
-if [ -n "$1" ]
-then
-    FROM_ENCODING=$1
-    TO_ENCODING="UTF-8"
-    OUTPUT="output_"
-    CONVERT=" iconv -f $FROM_ENCODING -t $TO_ENCODING"
-	for file in *.txt; do
-		$CONVERT -f $1 -o $1%.txt.utf8.converted
-	  done
-	exit 0
+Output="converted_"
+C_Charset="nullable"
+Convert="nullable"
+if [ -n "$1" ] #&& [ -n "$2" ]; 
+then 
+     echo "[++] Get of type entity ..."
+      C_Charset=$(file -i $1 | cut -d'=' -f2)
+    	if [ $C_Charset = "binary" ]; 
+    	then 
+    	      echo "Skip binary file. Abort ..."
+    	    exit 0
+    	else
+    	   echo "[++] Text File detected"
+    	     echo "[++] Converting $1 "
+    	       cat $1 | iconv -t latin1 | iconv -f cp1251 -t utf-8 >> ~/$Output$1
+    	       #iconv -f $C_Charset $1 -t $2 -o ~/$Output$1 
+    	   echo 'New file locate in' ~/$Output$1
+    	fi 
 else
-    echo "Incorrect agrument. Abort"
-    echo "Usage: ./encode.sh <path/<filename>.txt>"
+     echo 'Incorrect argument. You must entry filename'
+       echo 'Format to usage: ./encoder.sh /directory/filename'
+     echo 'For example: ./encoder.sh /tmp/example.txt'
+   exit 0 
 fi
