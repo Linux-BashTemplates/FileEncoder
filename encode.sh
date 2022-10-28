84238ea241111d6866a4055c848f1d919376914d
Sergey Zhyltsov <48886027+SergRikhter@users.noreply.github.com>
2022-10-28 11:55:24 +0300

    New functions

--- encode.sh
+++ encode.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 Output="converted_"
 C_Charset="nullable"
-Convert="nullable"
-if [ -n "$1" ] #&& [ -n "$2" ]; 
+if [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ] && [ -n "$4" ]; 
 then 
-     echo "[++] Get of type entity ..."
-      C_Charset=$(file -i $1 | cut -d'=' -f2)
-    	if [ $C_Charset = "binary" ]; 
-    	then 
-    	      echo "Skip binary file. Abort ..."
-    	    exit 0
-    	else
-    	   echo "[++] Text File detected"
-    	     echo "[++] Converting $1 "
-    	       cat $1 | iconv -t latin1 | iconv -f cp1251 -t utf-8 >> ~/$Output$1
-    	       #iconv -f $C_Charset $1 -t $2 -o ~/$Output$1 
-    	   echo 'New file locate in' ~/$Output$1
-    	fi 
+     if test -f ~/$Output$1 ; then 
+     	  echo "Delete previous file version" 
+     	rm ~/$Output$1
+     fi
+     	echo "[++] Get of type entity ..."
+      	  C_Charset=$(file -i $1 | cut -d'=' -f2)
+    	     if [ $C_Charset = "binary" ]; 
+    	     then 
+    	      	 echo "Skip binary file. Abort ..."
+    	     exit 0
+    	     else
+    	      echo "[++] Text File detected"
+    	      	echo "[++] Converting $1 "
+    	               cat $1 | iconv -t $2 | iconv -f $3 -t $4 >> ~/$Output$1
+    	             echo "Encode result:"
+    	             echo 
+    	               cat -n ~/$Output$1
+    	          echo
+    	      echo "Sucessfull... Bye"
+    	   fi 
 else
-     echo 'Incorrect argument. You must entry filename'
-       echo 'Format to usage: ./encoder.sh /directory/filename'
-     echo 'For example: ./encoder.sh /tmp/example.txt'
+    echo 'Incorrect argument: '
+        echo 'Reason: You must entry path to filename and set charsets for encoding text '
+        echo 'Format string: ./encode.sh /directory/filename charset_1 charset_2 charset_3'
+    echo 'For example: ./encode.sh /tmp/example.txt'
    exit 0 
 fi
+
